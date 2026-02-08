37d25fa4170fa44a13d59c0b9a0ed9b3ad0c7e0c
vocicassino <vocidicassino@proton.me>
2026-02-08 08:52:26 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "segnalafacile-demo-v1";
+const CACHE_NAME = "segnalafacile-v1";
 const ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./icon-192.png",
-  "./icon-512.png"
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
